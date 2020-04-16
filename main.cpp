cbced60c395ba97095f94b8c58ca51e0c1b1e79a
minkwon1114 <minkwon1114@gmail.com>
2020-04-16 03:05:19 -0700

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include "sorting.h"
+#include "stdio.h"
 
 int sorted[10];
 
 int main(){
-
+    int data[10];
+    for (int i=0;i<10;i++){
+        scanf("%d",&data[i]);
+    }
+    merge_sort(data,0,9);
+    for (int k=0;k<10;k++){
+        printf("%d ", sorted[k]);
+     }
     return 0;
 }
